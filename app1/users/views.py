565c37ee3bd8f20324905eaa23fae4ba7b2ed044
mary lazareva <lazareva.m1989@yandex.ru>
2024-05-11 15:51:13 +0300

    Profile form, controller & notifications are ready

--- app1/users/views.py
+++ app1/users/views.py
@@ -1,10 +1,12 @@
-from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth, messages
 from django.http import HttpResponse, HttpResponseRedirect
 from django.http.response import HttpResponseRedirectBase
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from users.forms import UserLoinForm, UserRegistrationForm
+
+from users.forms import ProfileForm, UserLoinForm, UserRegistrationForm
 
 
 def login(request):
@@ -16,6 +18,7 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
+                messages.success(request, f"{username}, Вы вошли в аккаунт")
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoinForm()
@@ -34,7 +37,7 @@ def registration(request):
             # Чтобы прирегистрации стразу заходить в личный кабинет без введения своих данных снова
             user = form.instance
             auth.login(request, user)
-
+            messages.success(request, f"{user.username}, Вы успешно зарагистрированы и вошли в аккаунт")
             return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserRegistrationForm()
@@ -46,13 +49,28 @@ def registration(request):
  
     return render(request, "users/registration.html", context)
 
-
+@login_required
 def profile(request):
-    context = {"title": "Home - Кабинет"}
+    if request.method == 'POST':
+        form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES) # используем instance, для какого пользователя нужно сохранять информацию
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Профайл успешно обновлен")
+            return HttpResponseRedirect(reverse('user:profile'))
+    else:
+        # добавляем instance, чтобы была информация, которая за ним числилась
+        form = ProfileForm(instance=request.user)
+
+    context = {
+            "title": "Home - Кабинет",
+            "form": form,
+            }
+ 
     return render(request, "users/profile.html", context)
 
-
+@login_required
 def logout(request):
+    messages.success(request, f"{request.user.username}, Вы вышли из аккаунта")
     auth.logout(request)
     return redirect(reverse('main:index'))
 
