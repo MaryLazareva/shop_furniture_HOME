3c58242bbc63ca44a37d6fa53d3e6763f2516f8d
mary lazareva <lazareva.m1989@yandex.ru>
2024-05-17 11:28:39 +0300

    Add cart view, refactor included cart templates

---
+++ app1/carts/templatetags/carts_tags.py
@@ -0,0 +1,11 @@
+from django import template
+
+from carts.models import Cart
+
+register = template.Library()
+
+@register.simple_tag()
+def user_carts(request):
+    # Выбираем все корзины этого пользователя
+    if request.user.is_authenticated:
+        return Cart.objects.filter(user=request.user)
