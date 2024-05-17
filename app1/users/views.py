3dee705fbc403fd2256b2363729a647a0521db46
mary lazareva <lazareva.m1989@yandex.ru>
2024-05-17 23:49:29 +0300

    Cart remove controller added, commit before ajax

--- app1/users/views.py
+++ app1/users/views.py
@@ -21,7 +21,7 @@ def login(request):
                 messages.success(request, f"{username}, Вы вошли в аккаунт")
 
                 redirect_page = request.POST.get('next', None)
-                if redirect_page and  redirect_page != reverse('user:logout'):
+                if redirect_page and redirect_page != reverse('user:logout'):
                     return HttpResponseRedirect(request.POST.get('next'))
                 
                 return HttpResponseRedirect(reverse('main:index'))
@@ -74,7 +74,7 @@ def profile(request):
     return render(request, "users/profile.html", context)
 
 def users_cart(request):
-    return render(request, 'users/users-cart.html')
+    return render(request, 'users/users_cart.html')
 
 
 @login_required
